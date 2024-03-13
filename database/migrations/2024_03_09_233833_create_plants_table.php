a4c720875cced53b81cb85d7b7464bb18c958745
Aaron Pierson <me@aaronpierson.me>
2024-03-12 21:20:27 -0500

    removing unnecessary cols

--- database/migrations/2024_03_09_233833_create_plants_table.php
+++ database/migrations/2024_03_09_233833_create_plants_table.php
@@ -34,24 +34,19 @@ return new class extends Migration
             $table->text('care_instructions')->nullable();
             $table->text('propagation_methods')->nullable(); // New: Methods of propagation (seeds, cuttings, etc.)
             $table->text('notes')->nullable();
+            $table->enum('status', ['alive', 'dead', 'dormant'])->default('alive');
+            $table->enum('difficulty', ['easy', 'medium', 'hard'])->default('easy');
             $table->text('description')->nullable();
             $table->integer('quantity')->nullable();
             $table->boolean('is_fruiting')->default(false); // New: Whether the plant bears fruit
             $table->boolean('is_edible')->default(false); // New: Whether the plant is edible
-            $table->string('flower_color')->nullable(); // New: Color of the flowers
-            $table->string('leaf_color')->nullable(); // New: Color of the leaves
             $table->string('seasonality')->nullable(); // New: When the plant typically grows or blooms
             $table->boolean('is_indoor')->default(false); // New: Whether the plant is typically grown indoors
             $table->boolean('is_perennial')->default(false); // New: Whether the plant is perennial
-            $table->float('height')->nullable(); // New: Typical height of the plant
-            $table->integer('width')->nullable(); // New: Typical width or spread of the plant
-            $table->string('fruit_color')->nullable(); // New: Color of the fruit, if applicable
             $table->string('bloom_time')->nullable(); // New: When the plant typically blooms
             $table->string('harvest_time')->nullable(); // New: When the fruit or produce is typically harvested
             $table->boolean('is_medicinal')->default(false); // New: Whether the plant has medicinal uses
             $table->text('medicinal_uses')->nullable(); // New: Description of medicinal uses, if applicable
-            $table->boolean('is_poisonous')->default(false); // New: Whether the plant is poisonous
-            $table->text('poisonous_treatment')->nullable(); // New: Description of treatment if ingested or touched, if poisonous
         });
     }
 

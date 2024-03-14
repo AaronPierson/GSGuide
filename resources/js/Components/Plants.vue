e18d517babb60acdd3e8eba10be5209b3e203951
Aaron Pierson <me@aaronpierson.me>
2024-03-13 20:00:19 -0500

    plants style

---
+++ resources/js/Components/Plants.vue
@@ -0,0 +1,64 @@
+<script setup>
+    import { Link } from '@inertiajs/vue3'
+
+    const props = defineProps({
+        plants: {
+            type: Array,
+            required: true
+        }
+    });
+
+</script>
+<template>
+    <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 m-5">
+        <div v-for="plant in plants" :key="plant.id" class="rounded m-5 shadow">
+            <div class="card w-72 bg-base-100 shadow-xl">
+            <!-- <img :src="plant.image" alt="Plant Image" class="w-full h-auto rounded" /> -->
+            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                <div class="card-body">
+                    <h2 class="card-title">
+                        {{ plant.name }}
+                    </h2>
+                    <!-- <p>                        
+                        {{ plant.description }}
+                    </p> -->
+                    <div class="card-actions justify-end">
+                        
+                        <!-- <div class="badge badge-outline">Fashion</div> 
+                        <div class="badge badge-outline">Products</div> -->
+                    </div>
+                    <div>
+                        <div class="mt-2">
+                            <div class="flex items-center mb-1">
+                                <span class="font-bold mr-1">Zone:</span>
+                                <span>{{ plant.zone }}</span>
+                            </div>
+                            <div class="flex items-center">
+                                <span class="font-bold mr-1">Difficulty:</span>
+                                <span>{{ plant.difficulty }}</span>
+                            </div>
+                            <div class="flex items-center">
+                                {{ plant.light }}
+                            </div>
+                        </div>
+                        <div class="flex items-center mt-2">
+                                <div class="flex items-center mr-2">
+                                    <i class="fas fa-sun mr-1"></i>
+                                    <span>{{ plant.sun }}</span>
+                                </div>
+                                <div class="flex items-center">
+                                    <i class="fas fa-tint mr-1"></i>
+                                    <span>{{ plant.water }}</span>
+                                </div>
+                        </div>
+                    </div>
+
+                </div>
+                <Link :href="route('plants.show', plant.id)" class="bg-blue-500 text-white p-2 rounded">
+                    View Plant
+                </Link>
+            </div>
+        </div>
+
+    </div>
+</template>
--- resources/js/Pages/Plants/Index.vue
+++ resources/js/Pages/Plants/Index.vue
@@ -1,72 +1,30 @@
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
-import { Link } from '@inertiajs/vue3'
+    import AppLayout from '@/Layouts/AppLayout.vue';
+    import { Link } from '@inertiajs/vue3'
+    import Plants from '@/Components/Plants.vue'
 
-const props = defineProps({
-    plants: {
-        type: Array,
-        required: true
-    }
-});
+    const props = defineProps({
+        plants: {
+            type: Array,
+            required: true
+        }
+    });
 
 </script>
 <template>
     <AppLayout>
-        <div>
-            <h1 class="m-5 text-5xl font-bold">Plant Index</h1>
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                Plants
+            </h2>
+        </template>
 
-                <div v-for="plant in plants" :key="plant.id" class="p-4 rounded shadow">
-
-                    <div class="card w-96 bg-base-100 shadow-xl">
-                    <!-- <img :src="plant.image" alt="Plant Image" class="w-full h-auto rounded" /> -->
-                    <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                        <div class="card-body">
-                            <h2 class="card-title">
-                                {{ plant.name }}
-                            </h2>
-                            <p>                        
-                                {{ plant.description }}
-                            </p>
-                            <div class="card-actions justify-end">
-                                
-                                <div class="badge badge-outline">Fashion</div> 
-                                <div class="badge badge-outline">Products</div>
-                            </div>
-                            <div>
-                                <div class="mt-2">
-                                    <div class="flex items-center mb-1">
-                                        <span class="font-bold mr-1">Zone:</span>
-                                        <span>{{ plant.zone }}</span>
-                                    </div>
-                                    <div class="flex items-center">
-                                        <span class="font-bold mr-1">Difficulty:</span>
-                                        <span>{{ plant.difficulty }}</span>
-                                    </div>
-                                    <div class="flex items-center">
-                                        {{ plant.light }}
-                                    </div>
-                                </div>
-                                <div class="flex items-center mt-2">
-                                        <div class="flex items-center mr-2">
-                                            <i class="fas fa-sun mr-1"></i>
-                                            <span>{{ plant.sun }}</span>
-                                        </div>
-                                        <div class="flex items-center">
-                                            <i class="fas fa-tint mr-1"></i>
-                                            <span>{{ plant.water }}</span>
-                                        </div>
-                                </div>
-                            </div>
-
-                        </div>
-                        <Link :href="route('plants.show', plant.id)" class="bg-blue-500 text-white p-2 rounded">
-                            View Plant
-                        </Link>
+            <div class="py-12">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+                        <Plants :plants="plants" />
                     </div>
                 </div>
-
             </div>
-        </div>
     </AppLayout>
 </template>
